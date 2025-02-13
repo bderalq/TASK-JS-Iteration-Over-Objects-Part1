4c477eb66915c2c055c4b91b3fc9feab6738e947
bderalq <balqallaf12@gmail.com>
2025-02-13 11:53:00 +0300

    final

--- objectIteration.js
+++ objectIteration.js
@@ -32,31 +32,21 @@ const movies = [
   },
 ];
 
-// Q1: Write a `hasKey` function that takes two arguments, an object and a key.
-// The function should return true if the object contains  the specified key, and false otherwise.
 function hasKey(obj, key) {
-  // write your code here...
+  return key in obj;
 }
 
-// Q2: Write a `printMovieTitles` function that accepts an array of objects called 'movies'
-// and iterates through it to print the titles of all the movies.
 function printMovieTitles(movies) {
-  // write your code here...
+  movies.forEach( m => console.log(m.title));
 }
-
-// Q3: Write a `countMoviesByYear` function that accepts an array of objects called 'movies'
-// and a year
-// and returns the number of movies that were released in that year.
 function countMoviesByYear(movies, year) {
-  // write your code here...
+return movies.filter(m => m.year == year).length;
 }
 
-// Q4: Write a `updateMovieGenre` function that accepts a title and a new genre
-// and updates the genre of the given movie, if it exists,
-// then returns the modified array of movies.
-// Hint: use array iterator .find
 function updateMovieGenre(movies, title, newGenre) {
-  // write your code here...
+  let movie = movies.find(movie => movie.title == title);
+  if(movie) {movie.genre = newGenre};
+  return movies;
 }
 
 module.exports = {
